a71a9486e312e51d7515774d42af7016e9e9b384
szuluaga-beep <steven000069@hotmail.com>
2024-11-12 00:58:54 -0500

    feat: creatting methods CRUD

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get("/products", function () {
+    return "Products list";
+});
+
+Route::get("/products/{id}", function () {
+    return "Return one product";
+});
+
+Route::post("/products", function () {
+    return "Creando producto";
+});
+
+Route::patch("/products/{id}", function () {
+    return "Actualizando producto";
+});
+
+Route::delete("/products/{id}", function () {
+    return "Deleting product ";
+});
